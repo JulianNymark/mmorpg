67b0216b619c4db4c689b808f55284bf8c14567f
julian nymark <julianymark@gmail.com>
2017-04-01 02:37:10 +0200

    flyway

---
+++ server/sql/R__Schema_api.sql
@@ -0,0 +1,21 @@
+---- stay fresh  ----
+
+DROP SCHEMA IF EXISTS api;
+CREATE SCHEMA api;
+
+DROP USER IF EXISTS 'mmorpg-api';
+CREATE USER 'mmorpg-api';
+
+---- functions ----
+
+CREATE OR REPLACE FUNCTION api.move (
+    p_input jsonb
+) RETURNS jsonb AS $$
+    BEGIN
+        RETURN p_input;
+    END;
+$$ LANGUAGE plpgsql;
+
+---- grants ----
+
+GRANT EXECUTE ON api.move(jsonb) TO 'mmorpg-api';
